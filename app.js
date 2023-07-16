6a88df264bf11fff77472bdd9139977eced4e1f9
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-16 13:44:28 +0100

    update

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express')
 const path = require('path')
 const app = express()
 app.use(express.static('./public'))
-app.get('/', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname,  'index.html'))
-})
-app.all('*', (req, res) => {
 
+app.get('/', (req, res) =>{
+    res.status(200).sendFile(path.resolve(__dirname, './index.html'))
+})
+app.all('*', (req, res) =>{
+    res.status(400).send('Opps! file does not exist')
 })
 
-app.listen(6000, () => {
-    console.log('App Running on port 6000');
+app.listen(5000, () =>{
+    console.log('App is currently lsitening on port 5000');
 })
