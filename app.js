2fb3d101ecb8d4f403f3a81a46f26db2086f66a6
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-16 12:08:31 +0100

    update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const path = require(path)
+const path = require('path')
 const app = express()
 app.get('/', (req, res) => {
-res.sendFile(path.resolve(__dirname, '.navbar-with-node js', 'index.html'))
+    res.status(200).sendFile(path.resolve(__dirname,  'index.html'))
 })
 app.all('*', (req, res) => {
 
